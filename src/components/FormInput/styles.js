6eb1834dc3bfbf2c43b58d681701d1f3018d67f5
emachipana <emachipana0102@gmail.com>
2022-06-22 00:19:24 -0500

    FIX: styles of input

--- src/components/FormInput/styles.js
+++ src/components/FormInput/styles.js
@@ -5,6 +5,7 @@ export const InputContainer = styled.div`
   max-width: 320px;
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   gap: 4px;
 `;
 
@@ -14,15 +15,21 @@ export const Label = styled.label`
   line-height: 16px;
   letter-spacing: 0.125em;
   text-transform: uppercase;
-  color: ${colors.gray[500]};
+  color: ${colors.gray[800]};
 `;
 
 export const Input = styled.input`
+  box-sizing: border-box;
+  font-family: ${fonts.primary};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
   background-color: white;
-  width: 100%;
+  width: 320px;
   outline: none;
   border: 1px solid ${colors.gray[200]};
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
   border-radius: 6px;
   color: ${colors.gray[600]};
+  padding: 8px 12px;
 `;
